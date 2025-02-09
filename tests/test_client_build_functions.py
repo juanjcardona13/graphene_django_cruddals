3f134aacc61baa63abdb55bfbb0f182fc53841cb
Juan Guzmán <juanjcardona13@gmail.com>
2025-02-09 07:36:32 -0500

    Refactor assertions in BuilderCruddalsApp and enhance test structure for client build functions

--- tests/test_client_build_functions.py
+++ tests/test_client_build_functions.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 from graphene_django_cruddals.client.build_functions import (
     PATH_CLIENT,
@@ -13,55 +14,67 @@ def normalize_text(text):
     return text.replace("behaviour", "behavior")
 
 
-def test_build_files_for_client_schema_cruddals():
-    build_files_for_client_schema_cruddals(schema)
+class TestClientBuildFunctions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        build_files_for_client_schema_cruddals(schema)
+        assert os.path.exists(PATH_CLIENT)
 
-    assert os.path.exists(PATH_CLIENT)
+    @classmethod
+    def tearDownClass(cls):
+        os.system(f"rm -rf {PATH_CLIENT}")
 
-    with open(f"{PATH_CLIENT}/general_types.js", encoding="utf-8") as file:
-        with open(
-            "tests/schema_client_js/schema_cruddals/general_types.js", encoding="utf-8"
-        ) as expected_file:
-            assert file.read().strip() == expected_file.read().strip()
+    def test_build_general_types_js(self):
+        with open(f"{PATH_CLIENT}/general_types.js", encoding="utf-8") as file:
+            with open(
+                "tests/schema_client_js/schema_cruddals/general_types.js",
+                encoding="utf-8",
+            ) as expected_file:
+                assert file.read().strip() == expected_file.read().strip()
 
-    with open(f"{PATH_CLIENT}/mutations.js", encoding="utf-8") as file:
-        with open(
-            "tests/schema_client_js/schema_cruddals/mutations.js", encoding="utf-8"
-        ) as expected_file:
-            assert file.read().strip() == expected_file.read().strip()
+    def test_build_mutations_js(self):
+        with open(f"{PATH_CLIENT}/mutations.js", encoding="utf-8") as file:
+            with open(
+                "tests/schema_client_js/schema_cruddals/mutations.js", encoding="utf-8"
+            ) as expected_file:
+                assert file.read().strip() == expected_file.read().strip()
 
-    with open(f"{PATH_CLIENT}/queries.js", encoding="utf-8") as file:
-        with open(
-            "tests/schema_client_js/schema_cruddals/queries.js", encoding="utf-8"
-        ) as expected_file:
-            assert file.read().strip() == expected_file.read().strip()
+    def test_build_queries_js(self):
+        with open(f"{PATH_CLIENT}/queries.js", encoding="utf-8") as file:
+            with open(
+                "tests/schema_client_js/schema_cruddals/queries.js", encoding="utf-8"
+            ) as expected_file:
+                assert file.read().strip() == expected_file.read().strip()
 
-    with open(f"{PATH_CLIENT}/schema-introspect.gql", encoding="utf-8") as file:
-        with open(
-            "tests/schema_client_js/schema_cruddals/schema-introspect.gql",
-            encoding="utf-8",
-        ) as expected_file:
-            assert normalize_text(file.read().strip()) == normalize_text(
-                expected_file.read().strip()
-            )
+    def test_build_schema_introspect_gql(self):
+        with open(f"{PATH_CLIENT}/schema-introspect.gql", encoding="utf-8") as file:
+            with open(
+                "tests/schema_client_js/schema_cruddals/schema-introspect.gql",
+                encoding="utf-8",
+            ) as expected_file:
+                assert normalize_text(file.read().strip()) == normalize_text(
+                    expected_file.read().strip()
+                )
+                # for line1, line2 in zip(file.read().strip().splitlines(), expected_file.read().strip().splitlines()):
+                #     assert line1 == line2, f"Diferencia en la línea:\n{line1}\n{line2}"
 
-    # with open(f"{PATH_CLIENT}/schema.gql", encoding="utf-8") as file:
-    #     with open(
-    #         "tests/schema_client_js/schema_cruddals/schema.gql", encoding="utf-8"
-    #     ) as expected_file:
-    #         assert file.read().strip() == expected_file.read().strip()
+    def test_build_test_in_graphiql_gql(self):
+        with open(f"{PATH_CLIENT}/test_in_graphiql.gql", encoding="utf-8") as file:
+            with open(
+                "tests/schema_client_js/schema_cruddals/test_in_graphiql.gql",
+                encoding="utf-8",
+            ) as expected_file:
+                assert file.read().strip() == expected_file.read().strip()
 
-    # with open(f"{PATH_CLIENT}/schema.json", encoding="utf-8") as file:
-    #     with open(
-    #         "tests/schema_client_js/schema_cruddals/schema.json", encoding="utf-8"
-    #     ) as expected_file:
-    #         assert file.read().strip() == expected_file.read().strip()
 
-    with open(f"{PATH_CLIENT}/test_in_graphiql.gql", encoding="utf-8") as file:
-        with open(
-            "tests/schema_client_js/schema_cruddals/test_in_graphiql.gql",
-            encoding="utf-8",
-        ) as expected_file:
-            assert file.read().strip() == expected_file.read().strip()
+# with open(f"{PATH_CLIENT}/schema.gql", encoding="utf-8") as file:
+#     with open(
+#         "tests/schema_client_js/schema_cruddals/schema.gql", encoding="utf-8"
+#     ) as expected_file:
+#         assert file.read().strip() == expected_file.read().strip()
 
-    os.system(f"rm -rf {PATH_CLIENT}")
+# with open(f"{PATH_CLIENT}/schema.json", encoding="utf-8") as file:
+#     with open(
+#         "tests/schema_client_js/schema_cruddals/schema.json", encoding="utf-8"
+#     ) as expected_file:
+#         assert file.read().strip() == expected_file.read().strip()
