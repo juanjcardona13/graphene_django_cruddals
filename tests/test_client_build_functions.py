5b3bfe26ae7980b196af34a5a4c6e13ffd547666
Juan Guzmán <juanjcardona13@gmail.com>
2025-02-09 07:32:23 -0500

    Fix test for support to new version in graphql

--- tests/test_client_build_functions.py
+++ tests/test_client_build_functions.py
@@ -7,6 +7,12 @@ from graphene_django_cruddals.client.build_functions import (
 from tests.schema import schema
 
 
+def normalize_text(text):
+    # Add this because in this commit (https://github.com/graphql-python/graphql-core/commit/967d58d20831032ef30555d04103342833b58a15) the word "behaviour" was changed to "behavior"
+    # to match the American English spelling, literally the description of the commit is "Use American English"
+    return text.replace("behaviour", "behavior")
+
+
 def test_build_files_for_client_schema_cruddals():
     build_files_for_client_schema_cruddals(schema)
 
@@ -35,7 +41,9 @@ def test_build_files_for_client_schema_cruddals():
             "tests/schema_client_js/schema_cruddals/schema-introspect.gql",
             encoding="utf-8",
         ) as expected_file:
-            assert file.read().strip() == expected_file.read().strip()
+            assert normalize_text(file.read().strip()) == normalize_text(
+                expected_file.read().strip()
+            )
 
     # with open(f"{PATH_CLIENT}/schema.gql", encoding="utf-8") as file:
     #     with open(
